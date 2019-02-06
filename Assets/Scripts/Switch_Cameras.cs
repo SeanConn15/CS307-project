089a046c8330df450c794fbccb2e1ecb9581bf9e
Haoran Wang <brucehrwang@yahoo.com>
2019-02-06 17:42:54 -0500

    Starting scene and add design document
    added design docuement and worked on starting scene

---
+++ Assets/Scripts/Switch_Cameras.cs
@@ -0,0 +1,54 @@
+// Aurthor: Haoran Wang
+// Purpose: let player switch between different levels by pressing left or right arrow keys
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch_Cameras : MonoBehaviour
+{
+    public Camera[] cameraList;
+    private int currentCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentCamera = 0;
+        for (int i = 0; i < cameraList.Length; i++)
+        {
+          cameraList[i].gameObject.SetActive(false);
+        }
+
+        if (cameraList.Length > 0)
+        {
+          cameraList[0].gameObject.SetActive (true);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Right Arrow Key
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+          currentCamera++;
+          if (currentCamera < cameraList.Length)
+          {
+            cameraList[currentCamera - 1].gameObject.SetActive(false);
+            cameraList[currentCamera].gameObject.SetActive(true);
+          }
+        }
+
+        // Left Arrow Key
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && currentCamera >= 1)
+        {
+          currentCamera--;
+          if (currentCamera >= 0)
+          {
+            cameraList[currentCamera + 1].gameObject.SetActive(false);
+            cameraList[currentCamera].gameObject.SetActive(true);
+          }
+        }
+
+    }
+}
