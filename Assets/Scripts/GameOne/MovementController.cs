7b8496696625df5e6c3388aba37b2d0406d6fcc7
Xingyu Wang <brandon.xyw@gmail.com>
2019-02-10 15:11:55 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.DS_Store

---
+++ Assets/Scripts/GameOne/MovementController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// author: Xingyu Wang
+// Protagnist movement controller
+// left arror key: move left
+// right arror key: move right
+// space: jump
+// need implementation
+
+public class MovementController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
