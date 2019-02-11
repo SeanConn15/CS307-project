2d4da3aea77caa8cd0aaa0e19857ecfae1e4b30b
Christian Bortolotti <cbortolo@purdue.edu>
2019-02-10 23:33:22 -0500

    Button Interfacing 1
    Button interfacing between task receiving and console output

---
+++ Assets/Scenes/Menu/ExpandScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExpandScript : MonoBehaviour
+{
+
+    public Button ExpandMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ExpandMenu.onClick.AddListener(TaskOnClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void TaskOnClick()
+    {
+        Debug.Log("Expand button pressed");
+    }
+}
