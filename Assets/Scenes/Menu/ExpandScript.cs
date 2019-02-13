73362eb1771d88321626a2f2cf3955bba41c2114
Christian Bortolotti <cbortolo@purdue.edu>
2019-02-13 05:51:37 -0500

    Button Interfacing 2
    Christian Bortolotti - 2/13/19
    -ExpandMenu button activates and shows the menu, disables self
    -Menu's back button hides menu and reactivates ExpandMenu button
    -All buttons assigned on click tasks, set to log
    -Functionality of ExpandMenu and MenuBack present

--- Assets/Scenes/Menu/ExpandScript.cs
+++ Assets/Scenes/Menu/ExpandScript.cs
@@ -6,22 +6,58 @@ using UnityEngine.UI;
 public class ExpandScript : MonoBehaviour
 {
 
-    public Button ExpandMenu;
+    public GameObject ExpandMenu, MenuBack, MenuVMinus, MenuVPlus, MenuVolume;
 
     // Start is called before the first frame update
     void Start()
     {
-        ExpandMenu.onClick.AddListener(TaskOnClick);
+       
+        //Buttons must be referenced as game objects to use setActive, etc.
+        ExpandMenu = GameObject.Find("ExpandMenu");
+        MenuBack = GameObject.Find("MenuBack");
+        MenuVMinus = GameObject.Find("MenuVMinus");
+        MenuVPlus = GameObject.Find("MenuVPlus");
+        MenuVolume = GameObject.Find("MenuVolume");
+
+        //Menu buttons begin inactive
+        MenuBack.SetActive(false);
+        MenuVPlus.SetActive(false);
+        MenuVMinus.SetActive(false);
+        MenuVolume.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnExpandClick()
     {
-
+        Debug.Log("Expand button pressed.");
+        ExpandMenu.SetActive(false);
+        MenuBack.SetActive(true);
+        MenuVPlus.SetActive(true);
+        MenuVMinus.SetActive(true);
+        MenuVolume.SetActive(true);
     }
 
-    void TaskOnClick()
+    public void OnBackClick()
     {
-        Debug.Log("Expand button pressed");
+        Debug.Log("Back button pressed.");
+        MenuBack.SetActive(false);
+        MenuVPlus.SetActive(false);
+        MenuVMinus.SetActive(false);
+        MenuVolume.SetActive(false);
+        ExpandMenu.SetActive(true);
+    }
+
+    public void OnVMinusClick()
+    {
+        Debug.Log("Decrease Volume.");
+    }
+
+    public void OnVPlusClick()
+    {
+        Debug.Log("Increase Volume.");
+    }
+
+    public void OnVolumeClick()
+    {
+        Debug.Log("Volume Pressed, Toggle Mute.");
     }
 }
