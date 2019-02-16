1669ee80f20632a9179a156ae72f5654fd7d8466
Haoran Wang <brucehrwang@yahoo.com>
2019-02-16 14:06:54 -0500

    Fix minor issues and add credit scene
    1. Fixed camera positions.
    2. Fixed a bug where player can press right arrow to skip video but the audio will be messed up.
    3. Added another camera for credit with audio and visual effect.

---
+++ Assets/Scripts/Main Scene/PlayVideo.cs
@@ -0,0 +1,28 @@
+//Author: Haoran Wang
+//Purpose: To play intro video. Let the user to press space bar to skip if they wish.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayVideo : MonoBehaviour {
+
+    public GameObject videoPlayer;
+    public GameObject currentCamera;
+    public GameObject nextCamera;
+
+	// Use this for initialization
+	void Start () {
+        videoPlayer.SetActive(true);
+        nextCamera.SetActive(false);
+	}
+
+	// Update is called once per frame
+	void Update() {
+      if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.RightArrow)){
+        Destroy(videoPlayer);
+        currentCamera.SetActive(false);
+        nextCamera.SetActive(true);
+      }
+  }
+}
