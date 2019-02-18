4ae4fc207e02fe064cb8a3d1657079efb048f28d
Sean Connelly <connell7@purdue.edu>
2019-02-18 17:40:06 -0500

    Added final scene layout Still need to program dialog menu, but all collision scripts, camera locations, and interactable objects are here.

---
+++ Assets/Scenes/Customization/dialog_trigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dialog_trigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            Animator anim1 = other.GetComponent<Animator>();
+            Animator anim2 = GameObject.Find("NPC").GetComponent<Animator>();
+            anim1.SetTrigger("Wave");
+            anim2.SetTrigger("Wave");
+
+            Debug.Log("Hello friend!");
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            Debug.Log("Goodbye!");
+        }
+    }
+}
