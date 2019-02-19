939ebb40d5c636baa84961ab96cfc22bec291b92
Sean Connelly <connell7@purdue.edu>
2019-02-18 19:06:05 -0500

    intermediary commit added more functionality

--- Assets/Scenes/Customization/dialog_trigger.cs
+++ Assets/Scenes/Customization/dialog_trigger.cs
@@ -5,17 +5,25 @@ using UnityEngine;
 public class dialog_trigger : MonoBehaviour
 {
     // Start is called before the first frame update
+    GameObject player;
+    GameObject NPC;
+    GameObject dialog;
     private void Start()
     {
+        player = GameObject.Find("Player");
+        NPC = GameObject.Find("NPC");
+        dialog = GameObject.Find("Dialog");
+        dialog.SetActive(false);
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.name == "Player")
         {
-            Animator anim1 = other.GetComponent<Animator>();
-            Animator anim2 = GameObject.Find("NPC").GetComponent<Animator>();
-            anim1.SetTrigger("Wave");
+            //Animator anim1 = player.GetComponent<Animator>();
+            Animator anim2 = NPC.GetComponent<Animator>();
+            //anim1.SetTrigger("Wave");
             anim2.SetTrigger("Wave");
+            dialog.SetActive(true);
 
             Debug.Log("Hello friend!");
         }
@@ -24,6 +32,7 @@ public class dialog_trigger : MonoBehaviour
     {
         if (other.name == "Player")
         {
+            dialog.SetActive(false);
             Debug.Log("Goodbye!");
         }
     }
