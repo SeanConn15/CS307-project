ce804c6d06a98771d9f197647de493d998ebdd30
Christian Bortolotti <cbortolo@purdue.edu>
2019-02-19 23:40:13 -0500

    cstm_ui back
    -

---
+++ Assets/Scenes/Customization/Backup/texture_alter.cs
@@ -0,0 +1,50 @@
+﻿//Christian Bortolotti
+//Script for switching between textures
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class texture_alter : MonoBehaviour
+{
+
+    public Texture[] textureBank;
+    int currentTexture = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            OnRightArrow();
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            OnLeftArrow();
+        }
+    }
+
+    void OnRightArrow()
+    {
+        Debug.Log("Texture select right.");
+        currentTexture++;
+        if (currentTexture >= textureBank.Length)
+        {
+            currentTexture = 0;
+        }
+        //Renderer ren = GameObject.Find("Player").GetComponent<Renderer>();
+        //ren.material.mainTexture = textureBank[currentTexture];
+    }
+
+    void OnLeftArrow()
+    {
+        Debug.Log("Texture select left.");
+        currentTexture++;
+        if (currentTexture >= textureBank.Length)
+        {
+            currentTexture = 0;
+        }
+        //Renderer ren = GameObject.Find("Player").GetComponent<Renderer>();
+        //ren.material.mainTexture = textureBank[currentTexture];
+    }
+}
