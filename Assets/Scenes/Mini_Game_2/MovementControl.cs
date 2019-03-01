ffd1eff5d7a440b02993394fe08e23291077c5d2
Haoran Wang <brucehrwang@yahoo.com>
2019-03-01 14:19:38 -0500

    Start working on game 2
    1. Add plane and trees.
    2. Add movement controller.
    3. Add snow effect

---
+++ Assets/Scenes/Mini_Game_2/MovementControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementControl : MonoBehaviour
+{
+
+    public float speed;
+    CharacterController charControl;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      charControl = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+      float horiz = Input.GetAxis("Horizontal");
+     float vert = Input.GetAxis("Vertical");
+
+     Vector3 moveDirSide = transform.right * horiz * speed;
+     Vector3 moveDirForward = transform.forward * vert * speed;
+
+     charControl.SimpleMove(moveDirSide);
+     charControl.SimpleMove(moveDirForward);
+    }
+}
