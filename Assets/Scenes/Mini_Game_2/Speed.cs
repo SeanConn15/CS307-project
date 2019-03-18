9b1f0049eb163cf0bbd32063d6833c67ec998c27
Christian Bortolotti <cbortolo@purdue.edu>
2019-03-17 21:53:38 -0400

    HUD - Time and Speed
    Christian Bortolotti - 3/17/19
    -Added speed and time objects to the HUD
    -Created scripts to handle variable collection and updating

--- Assets/Scenes/Mini_Game_2/Speed.cs
+++ Assets/Scenes/Mini_Game_2/Speed.cs
@@ -1,18 +1,31 @@
-﻿using System.Collections;
+﻿//Christian Bortolotti - A script that obtains the speed of the rigidbody and updates the HUD
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Speed : MonoBehaviour
 {
+
+    public Text speedText = null;
+    public Rigidbody carRB;
+    private float tmp;
+    int tmp2;
+
     // Start is called before the first frame update
     void Start()
     {
+        //carRB = GetComponent<Rigidbody>();
+        speedText.text = "";
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        tmp = carRB.velocity.magnitude;
+        tmp2 = (int)tmp;
+        speedText.text = tmp2.ToString() + " mph";
     }
 }
