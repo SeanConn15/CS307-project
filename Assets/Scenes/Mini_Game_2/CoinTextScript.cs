8348e2b3287770d70d69f4114b67fa8a2a2bf542
Seunghyun Lee <lee2015@purdue.edu>
2019-03-20 15:58:20 -0400

    Game 2 Coin implementation
    Coin successfully mapped out and updates to UI.

---
+++ Assets/Scenes/Mini_Game_2/CoinTextScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinTextScript : MonoBehaviour
+{
+    public Text coinText;
+    public static int coinCount = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        coinText.text = "0";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        coinText.text = coinCount.ToString();
+    }
+}
