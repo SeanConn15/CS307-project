7846526c86cd7bca60d672cb1df348d6418a86f9
Christian Bortolotti <cbortolo@purdue.edu>
2019-04-15 20:39:55 -0400

    Countdown
    //Christian Bortolotti - 4/17/19
    -Added UI Countdown Element under existing canvas object
    -Created Script to Count from serialized number
    -Reloads level when count reaches 0
    -All canvas objects are rendered inactive upon reaching exit

---
+++ Assets/Scenes/Mini_Game_2/CountDown.cs
@@ -0,0 +1,64 @@
+﻿//Christian Bortolotti - Countdown script for the final part of game2
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CountDown : MonoBehaviour
+{
+
+    public Text CountDownTime;
+    bool x = true;
+    bool active = true;
+    float s;
+    [SerializeField] private float timeLimit;
+    [SerializeField] public float Radius;
+    public GameObject Canvas1;
+    public GameObject Player;
+    public GameObject Exit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        s = timeLimit;
+        Canvas1.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(Vector3.Distance(Player.transform.position, Exit.transform.position) < Radius)
+        {
+            active = false;
+            CountDownTime.text = "";
+            Canvas1.SetActive(false);
+        }
+        if (active)
+        {
+            if (s >= 0.0f)
+            {
+                s -= Time.deltaTime;
+                CountDownTime.text = s.ToString("F");
+            }
+            else
+            {
+                //Debug.Log("Time Ended, Reload.");
+                if (s < 0.0f && x)
+                {
+                    Doom();
+                    x = false;
+                }
+            }
+        }
+    }
+
+    //IEnumerator Doom()
+    void Doom()
+    {
+        //yield return new WaitForSeconds(1.5f);
+        SceneManager.LoadScene("Final");
+    }
+}
